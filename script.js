297db8b6e1bc9aaa02788b40da091cf2e5be48d2
vishwajeet-8 <109028770+vishwajeet-8@users.noreply.github.com>
2024-05-03 23:34:10 +0530

    complete

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ const cardNamePara = document.querySelector(".card-name-para");
 const cardNumberPara = document.querySelector(".card-number-para");
 const cardExpDatePara = document.querySelector(".card-exp-date-para");
 const cardCVVPara = document.querySelector(".card-cvv-para");
+let numbersOnly = /^[0-9 ]+$/;
+let lettersOnly = /^[a-zA-Z\s]+$/;
+const form = document.getElementsByTagName("form");
+const completeCard = document.querySelector(".complete-card");
+const continueBtn = document.querySelector(".ctn-btn");
+const name = document.querySelector("#name");
+const number = document.querySelector("#number");
+const expDateMonth = document.querySelector("#expiry-date-month");
+const expDateYear = document.querySelector("#expiry-date-year");
+const cvv = document.querySelector("#cvc");
+const error = document.querySelector(".error");
 
 function inputHandle(event) {
   // NAME
@@ -14,27 +25,32 @@ function inputHandle(event) {
     if (event.target.value == "") {
       cardNamePara.innerHTML = "can't be blank";
       event.target.style.border = "2px solid red";
-    } else {
+    } else if (lettersOnly.test(event.target.value)) {
       cardName.innerHTML = event.target.value;
       cardNamePara.innerHTML = "";
       event.target.style.border = "2px solid hsl(278, 68%, 11%)";
+    } else {
+      cardNamePara.innerHTML = "Enter valid name";
+      event.target.style.border = "2px solid red";
     }
   }
   // NUMBER
   else if (event.target.id == "number") {
-    if (event.target.value == "") {
+    let val = event.target.value;
+    if (val == "") {
       cardNumberPara.innerHTML = "can't be blank";
       event.target.style.border = "2px solid red";
-    } else {
-      let val = event.target.value;
-      if (val.length == 19) event.preventDefault();
-      if (val.length == 5 || val.length == 10 || val.length == 15) {
-        console.log(val);
-        val = val + " a";
-      }
-      cardNumber.innerHTML = val;
+    } else if (numbersOnly.test(val) && val.length <= 19) {
+      let inputValue = event.target.value.replace(/\D/g, ""); // Remove non-digit characters
+      let formattedValue = inputValue.replace(/(\d{4})(?=\d)/g, "$1 "); // Group digits into sets of four
+      event.target.value = formattedValue.trim();
+
+      cardNumber.innerHTML = event.target.value;
       cardNumberPara.innerHTML = "";
       event.target.style.border = "2px solid hsl(278, 68%, 11%)";
+    } else {
+      cardNumberPara.innerHTML = "Enter valid card number";
+      event.target.style.border = "2px solid red";
     }
   }
   // MONTH
@@ -42,10 +58,13 @@ function inputHandle(event) {
     if (event.target.value == "") {
       cardExpDatePara.innerHTML = "can't be blank";
       event.target.style.border = "2px solid red";
-    } else {
+    } else if (event.target.value > 0 && event.target.value <= 12) {
       cardDateMonth.innerHTML = event.target.value;
       cardExpDatePara.innerHTML = "";
       event.target.style.border = "2px solid hsl(278, 68%, 11%)";
+    } else {
+      cardExpDatePara.innerHTML = "Enter valid month";
+      event.target.style.border = "2px solid red";
     }
   }
   // YEAR
@@ -53,10 +72,13 @@ function inputHandle(event) {
     if (event.target.value == "") {
       cardExpDatePara.innerHTML = "can't be blank";
       event.target.style.border = "2px solid red";
-    } else {
+    } else if (event.target.value >= 24) {
       cardDateYear.innerHTML = event.target.value;
       cardExpDatePara.innerHTML = "";
       event.target.style.border = "2px solid hsl(278, 68%, 11%)";
+    } else {
+      cardExpDatePara.innerHTML = "Enter valid year";
+      event.target.style.border = "2px solid red";
     }
   }
   // CVV
@@ -64,10 +86,36 @@ function inputHandle(event) {
     if (event.target.value == "") {
       cardCVVPara.innerHTML = "can't be blank";
       event.target.style.border = "2px solid red";
-    } else {
+    } else if (event.target.value.length <= 3) {
       cardCvv.innerHTML = event.target.value;
       cardCVVPara.innerHTML = "";
       event.target.style.border = "2px solid hsl(278, 68%, 11%)";
+    } else {
+      cardCVVPara.innerHTML = "Enter valid CVV";
+      event.target.style.border = "2px solid red";
     }
   }
 }
+
+form[0].addEventListener("submit", (e) => {
+  if (
+    name.value == "" ||
+    number.value == "" ||
+    expDateMonth.value == "" ||
+    expDateYear.value == "" ||
+    cvv.value == ""
+  ) {
+    error.innerHTML = "please fill all details";
+    e.preventDefault();
+    return;
+  }
+  e.preventDefault();
+  e.target.style.display = "none";
+  completeCard.style.display = "flex";
+});
+
+continueBtn.addEventListener("click", (e) => {
+  e.target.style.display = "none";
+  form[0].style.display = "block";
+  location.reload();
+});
